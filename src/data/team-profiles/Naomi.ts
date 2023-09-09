26118fecaa8705f9f3f9cfff3651f1b16a74f438
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-09 10:54:43 -0700

    chore: update chat link (#188)

--- src/data/team-profiles/Naomi.ts
+++ src/data/team-profiles/Naomi.ts
@@ -15,7 +15,7 @@ export const Naomi: Staff = {
     {
       name: "Discord",
       icon: faDiscord,
-      url: "https://chat.naomi.lgbt",
+      url: "https://chat.nhcarrigan.com",
       color: "#FFFFFF",
       background: "#5865F2",
       hover: false,
